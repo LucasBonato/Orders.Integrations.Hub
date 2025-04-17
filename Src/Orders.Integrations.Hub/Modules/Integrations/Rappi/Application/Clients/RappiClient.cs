4e23106a4a61a43e60bc811a872630c3fd665374
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-04-17 05:31:33 -0300

    [FEAT] Start Rappi integration (migration)

---
+++ Src/Orders.Integrations.Hub/Modules/Integrations/Rappi/Application/Clients/RappiClient.cs
@@ -0,0 +1,8 @@
+﻿using Orders.Integrations.Hub.Modules.Integrations.Common.Contracts;
+
+namespace Orders.Integrations.Hub.Modules.Integrations.Rappi.Application.Clients;
+
+public class RappiClient : IIntegrationClient
+{
+    
+}
