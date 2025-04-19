43b93ae1243dd35880e0a6d0e204b3ea24991078
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-04-19 01:30:36 -0300

    [REFACTOR] Common class to generate sha256 signatures validation

--- Src/BizPik.Orders.Hub/Modules/Integrations/Ifood/Validators/IfoodSignatureValidator.cs
+++ Src/BizPik.Orders.Hub/Modules/Integrations/Ifood/Validators/IfoodSignatureValidator.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
-using System.Security.Cryptography;
+
+using BizPik.Orders.Hub.Modules.Integrations.Common.Validators;
+
 using static Microsoft.AspNetCore.Http.Results;
 
 namespace BizPik.Orders.Hub.Modules.Integrations.Ifood.Validators;
@@ -34,30 +36,11 @@ public class IfoodSignatureValidator(
             return BadRequest("Signature header is missing.");
         }
 
-        if (!IsSignatureValid(signature, body))
+        if (!signature.IsSignatureValid(signature, body))
         {
             return BadRequest("Invalid signature");
         }
 
         return await next(context);
-    }
-
-    private bool IsSignatureValid(string headerSignature, string body)
-    {
-        logger.LogInformation("[INFO] - IfoodSignatureValidator - Expected Signature: [{signature}]", headerSignature);
-
-        string generatedSignature = GetExpectedSignature(AppEnv.INTEGRATIONS.IFOOD.CLIENT.SECRET.NotNull(), body);
-
-        logger.LogInformation("[INFO] - IfoodSignatureValidator - Generated Signature: [{signature}]", generatedSignature);
-
-        return generatedSignature == headerSignature;
-    }
-
-    private static string GetExpectedSignature(string secret, string data)
-    {
-        using HMACSHA256 hmacSha256 = new (Encoding.UTF8.GetBytes(secret));
-        byte[] hmacBytes = hmacSha256.ComputeHash(Encoding.UTF8.GetBytes(data));
-        string hmacHex = Convert.ToHexStringLower(hmacBytes);
-        return hmacHex;
     }
 }
