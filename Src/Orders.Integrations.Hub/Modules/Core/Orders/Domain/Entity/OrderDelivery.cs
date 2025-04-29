74bb82f04fb289fc06643ce8e8c5d4cb33a64604
LucasBonato <lucas.perez.bonato@gmail.com>
2025-04-29 15:45:39 -0300

    [FEAT] Pickup code added to integrations

--- Src/Orders.Integrations.Hub/Modules/Core/Orders/Domain/Entity/OrderDelivery.cs
+++ Src/Orders.Integrations.Hub/Modules/Core/Orders/Domain/Entity/OrderDelivery.cs
@@ -8,5 +8,6 @@ public record OrderDelivery(
     [property: JsonPropertyName("deliveredBy")] [property: JsonConverter(typeof(JsonStringEnumConverter))] OrderDeliveredBy DeliveredBy,
     [property: JsonPropertyName("estimatedDeliveryDateTime")] DateTime EstimatedDeliveryDateTime,
     [property: JsonPropertyName("deliveryDateTime")] DateTime DeliveryDateTime,
-    [property: JsonPropertyName("deliveryAddress")] Address.Address? DeliveryAddress
+    [property: JsonPropertyName("deliveryAddress")] Address.Address? DeliveryAddress,
+    [property: JsonPropertyName("pickupCode")] string? PickupCode
 );
