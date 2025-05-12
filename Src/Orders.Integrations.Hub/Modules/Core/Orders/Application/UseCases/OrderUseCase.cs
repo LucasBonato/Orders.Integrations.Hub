a5bff6950870f234d14a1ed36cffb294a22b6d8d
LucasBonato <lucas.perez.bonato@gmail.com>
2025-05-12 13:54:30 -0300

    [FIX] Log order before sending it to orders api

--- Src/Orders.Integrations.Hub/Modules/Core/Orders/Application/UseCases/OrderUseCase.cs
+++ Src/Orders.Integrations.Hub/Modules/Core/Orders/Application/UseCases/OrderUseCase.cs
@@ -1,4 +1,6 @@
-﻿using Orders.Integrations.Hub.Modules.Core.Orders.Domain.Contracts;
+﻿using System.Text.Json;
+
+using Orders.Integrations.Hub.Modules.Core.Orders.Domain.Contracts;
 using Orders.Integrations.Hub.Modules.Core.Orders.Domain.Contracts.UseCases;
 using Orders.Integrations.Hub.Modules.Core.Orders.Domain.ValueObjects.Events;
 
@@ -11,6 +13,7 @@ public class OrderUseCase(
     public async Task CreateOrder(CreateOrderEvent order)
     {
         logger.LogInformation("[INFO] - CreateOrderEventHandler - Creating Order From {salesChannel}", order.SalesChannel);
+        logger.LogInformation("[INFO] - CreateOrderEventHandler - Order: {order}", JsonSerializer.Serialize(order.Order));
         await orderClient.CreateOrder(order.Order);
     }
 
