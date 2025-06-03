1304745af332d18ec2b820728723f5a471cebc9b
LucasBonato <lucas.perez.bonato@gmail.com>
2025-06-03 01:29:25 -0300

    [FEAT] Try to implement Dispute response in ifood

--- Src/Orders.Integrations.Hub/Core/Orders/Domain/ValueObjects/DTOs/Request/RespondDisputeIntegrationRequest.cs
+++ Src/Orders.Integrations.Hub/Core/Orders/Domain/ValueObjects/DTOs/Request/RespondDisputeIntegrationRequest.cs
@@ -5,6 +5,8 @@ using Orders.Integrations.Hub.Core.Orders.Domain.ValueObjects.Enums;
 namespace Orders.Integrations.Hub.Core.Orders.Domain.ValueObjects.DTOs.Request;
 
 public record RespondDisputeIntegrationRequest(
+    [property: JsonPropertyName("disputeId")] string DisputeId,
+    [property: JsonPropertyName("alternativeId")] string? AlternativeId,
     [property: JsonPropertyName("type")] DisputeResponseType Type,
     [property: JsonPropertyName("metadata")] Dictionary<string, object> Metadata
 );
---
+++ Src/Orders.Integrations.Hub/Integrations/Ifood/Application/Ports/IfoodHandshakeOrderDisputeRespondUseCase.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+using Orders.Integrations.Hub.Core.Orders.Domain.Contracts.UseCases;
+using Orders.Integrations.Hub.Core.Orders.Domain.ValueObjects.DTOs.Request;
+using Orders.Integrations.Hub.Core.Orders.Domain.ValueObjects.Enums;
+using Orders.Integrations.Hub.Integrations.Ifood.Domain.Contracts;
+using Orders.Integrations.Hub.Integrations.Ifood.Domain.ValueObjects.DTOs.Request;
+
+namespace Orders.Integrations.Hub.Integrations.Ifood.Application.Ports;
+
+public class IfoodHandshakeOrderDisputeRespondUseCase(
+    IIFoodClient ifoodClient
+) : IOrderDisputeRespondUseCase {
+    public async Task ExecuteAsync(RespondDisputeIntegrationRequest respondRequest)
+    {
+        return respondRequest.Type switch {
+            DisputeResponseType.ACCEPT => ifoodClient.PostHandshakeDisputesAccept(disputeId: respondRequest.DisputeId),
+            DisputeResponseType.REJECT => ifoodClient.PostHandshakeDisputesReject(disputeId: respondRequest.DisputeId),
+            DisputeResponseType.COUNTERPROPOSAL => ifoodClient.PostHandshakeDisputesAlternatives(
+                disputeId: respondRequest.DisputeId,
+                alternativeId: respondRequest.AlternativeId!,
+                request: new HandshakeAlternativeRequest(
+                    Metadata: JsonSerializer.Deserialize<HandshakeAlternativeMetadata>(JsonSerializer.Serialize(respondRequest.Metadata))?? throw new Exception(),
+                    Type:
+                )
+            ),
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
