22592bd0a2881bb17e63d638b89740b23033299f
LucasBonato <lucas.perez.bonato@gmail.com>
2025-06-16 20:00:49 -0300

    [REFACTOR] Change IFormFile to Stream of bytes

--- Src/BizPik.Orders.Hub/Core/Domain/Contracts/IObjectStorageClient.cs
+++ Src/BizPik.Orders.Hub/Core/Domain/Contracts/IObjectStorageClient.cs
@@ -1,7 +1,7 @@
 ﻿namespace BizPik.Orders.Hub.Core.Domain.Contracts;
 
 public interface IObjectStorageClient {
-    Task<string> UploadFile(IFormFile file, string key);
+    Task<string> UploadFile(Stream file, string contentType, string key);
     Task DeleteFile(string key);
     Task DeleteFolder(string pathKey);
 }
