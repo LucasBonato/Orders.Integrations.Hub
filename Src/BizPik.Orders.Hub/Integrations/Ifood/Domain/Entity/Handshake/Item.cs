38bb7d97e28bed7eaac434f18046c1035d049f46
LucasBonato <lucas.perez.bonato@gmail.com>
2025-06-24 11:33:23 -0300

    [FIX] Ifood is sending integrationId instead of externalId

--- Src/BizPik.Orders.Hub/Integrations/Ifood/Domain/Entity/Handshake/Item.cs
+++ Src/BizPik.Orders.Hub/Integrations/Ifood/Domain/Entity/Handshake/Item.cs
@@ -5,7 +5,8 @@ namespace BizPik.Orders.Hub.Integrations.Ifood.Domain.Entity.Handshake;
 public record Item(
     [property: JsonPropertyName("id")] string Id,
     [property: JsonPropertyName("uniqueId")] string UniqueId,
-    [property: JsonPropertyName("externalCode")] string ExternalCode,
+    [property: JsonPropertyName("externalCode")] string? ExternalCode,
+    [property: JsonPropertyName("integrationId")] string? IntegrationId,
     [property: JsonPropertyName("quantity")] int Quantity,
     [property: JsonPropertyName("index")] int Index,
     [property: JsonPropertyName("amount")] Amount Amount,
