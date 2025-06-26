26a194f12b6dfb30e7396ea131943ff062bff568
LucasBonato <lucas.perez.bonato@gmail.com>
2025-06-26 09:02:32 -0300

    [FEAT] Use the cache service when requesting the token

--- Src/Orders.Integrations.Hub/Integrations/Rappi/Application/Handlers/RappiAuthMessageHandler.cs
+++ Src/Orders.Integrations.Hub/Integrations/Rappi/Application/Handlers/RappiAuthMessageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Orders.Integrations.Hub.Core.Application.Extensions;
+using Orders.Integrations.Hub.Integrations.Common.Contracts;
 using Orders.Integrations.Hub.Integrations.Rappi.Application.Clients;
 using Orders.Integrations.Hub.Integrations.Rappi.Domain.ValueObjects.DTOs.Request;
 using Orders.Integrations.Hub.Integrations.Rappi.Domain.ValueObjects.DTOs.Response;
@@ -7,25 +8,35 @@ namespace Orders.Integrations.Hub.Integrations.Rappi.Application.Handlers;
 
 public class RappiAuthMessageHandler(
     ILogger<RappiAuthMessageHandler> logger,
-    RappiAuthClient rappiAuthClient
+    RappiAuthClient rappiAuthClient,
+    ICacheService cacheService
 ) : DelegatingHandler {
+    private readonly string CACHE_KEY = AppEnv.INTEGRATIONS.RAPPI.CACHE.KEY.NotNullEnv();
+
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         if (request.Headers.Authorization is not null) {
             return await base.SendAsync(request, cancellationToken);
         }
 
-        RappiAuthTokenResponse token = await rappiAuthClient.RetrieveToken(
-            new RappiAuthTokenRequest(
-                ClienteId: AppEnv.INTEGRATIONS.RAPPI.CLIENT.ID.NotNullEnv(),
-                ClienteSecret: AppEnv.INTEGRATIONS.RAPPI.CLIENT.SECRET.NotNullEnv(),
-                Audience: AppEnv.INTEGRATIONS.RAPPI.CLIENT.AUDIENCE.NotNullEnv()
-            )
+        string accessToken = await cacheService.GetOrSetTokenAsync(
+            CACHE_KEY,
+            nameof(RappiAuthMessageHandler),
+            logger,
+            async () => {
+                RappiAuthTokenResponse token = await rappiAuthClient.RetrieveToken(
+                    new RappiAuthTokenRequest(
+                        ClienteId: AppEnv.INTEGRATIONS.RAPPI.CLIENT.ID.NotNullEnv(),
+                        ClienteSecret: AppEnv.INTEGRATIONS.RAPPI.CLIENT.SECRET.NotNullEnv(),
+                        Audience: AppEnv.INTEGRATIONS.RAPPI.CLIENT.AUDIENCE.NotNullEnv()
+                    )
+                );
+
+                return (token.AccessToken, TimeSpan.FromSeconds(token.ExpiresIn));
+            }
         );
 
-        logger.LogWarning("[INFO] - RappiAuthMessageHandler - Generating new Access token");
-
-        request.Headers.Add("x-authorization", $"{token.TokenType} {token.AccessToken}");
+        request.Headers.Add("x-authorization", $"Bearer {accessToken}");
 
         return await base.SendAsync(request, cancellationToken);
     }
