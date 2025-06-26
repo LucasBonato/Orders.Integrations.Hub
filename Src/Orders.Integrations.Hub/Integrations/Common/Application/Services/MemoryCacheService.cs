a174b21809aab090132d52eb3dc25b7dc2950ada
LucasBonato <lucas.perez.bonato@gmail.com>
2025-06-26 09:01:43 -0300

    [FEAT] In memory cache service implementation

---
+++ Src/Orders.Integrations.Hub/Integrations/Common/Application/Services/MemoryCacheService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Orders.Integrations.Hub.Integrations.Common.Application.Services;
+
+public class MemoryCacheService(
+    IMemoryCache memoryCache
+) : BaseCacheService {
+    public override T? TryGet<T>(string key) where T : default
+        => memoryCache.TryGetValue(key, out T? value) ? value : default;
+
+    public override ValueTask SetAsync<T>(string key, T value, TimeSpan expiration) {
+        memoryCache.Set(key, value, expiration);
+        return ValueTask.CompletedTask;
+    }
+}
--- Src/Orders.Integrations.Hub/Integrations/IntegrationsModule.cs
+++ Src/Orders.Integrations.Hub/Integrations/IntegrationsModule.cs
@@ -1,4 +1,6 @@
-﻿using Orders.Integrations.Hub.Integrations.Ifood;
+﻿using Orders.Integrations.Hub.Integrations.Common.Application.Services;
+using Orders.Integrations.Hub.Integrations.Common.Contracts;
+using Orders.Integrations.Hub.Integrations.Ifood;
 using Orders.Integrations.Hub.Integrations.Ifood.Adapter;
 using Orders.Integrations.Hub.Integrations.Rappi;
 using Orders.Integrations.Hub.Integrations.Rappi.Adapter;
@@ -10,16 +12,30 @@ public static class IntegrationsModule
     public static IServiceCollection AddIntegrationsModule(this IServiceCollection services)
     {
         return services
-            .AddIfood()
-            .AddRappi()
-        ;
+                .AddIfood()
+                .AddRappi()
+            ;
     }
 
     public static IApplicationBuilder UseIntegrationsModule(this WebApplication app)
     {
         return app
-            .AddIfoodEndpoints()
-            .AddRappiEndpoints()
-        ;
+                .UseIfoodEndpoints()
+                .UseRappiEndpoints()
+            ;
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services
+                .AddSingleton<ICacheService, MemoryCacheService>()
+            ;
+    }
+
+    private static IServiceCollection AddCacheServices(this IServiceCollection services)
+    {
+        return services
+                .AddMemoryCache()
+            ;
     }
 }
