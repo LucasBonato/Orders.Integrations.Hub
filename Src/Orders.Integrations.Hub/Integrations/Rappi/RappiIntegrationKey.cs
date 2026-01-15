5404b6857e193d9d82b4045e042431fa6f648c40
LucasBonato <lucas.perez.bonato@gmail.com>
2026-01-15 19:46:23 -0300

    [FEAT] Implement IntegrationKey in the Rappi integration

---
+++ Src/Orders.Integrations.Hub/Integrations/Rappi/RappiIntegrationKey.cs
@@ -0,0 +1,13 @@
+﻿using Orders.Integrations.Hub.Core.Application.Integration;
+using Orders.Integrations.Hub.Integrations.Common.Validators;
+
+namespace Orders.Integrations.Hub.Integrations.Rappi;
+
+public static class RappiIntegrationKey {
+    public const string Value = "RAPPI";
+    public static readonly IntegrationKey RAPPI = IntegrationKey.From(Value);
+
+    static RappiIntegrationKey() {
+        IntegrationKeyValidator.ValidateRawValue(Value);
+    }
+}
