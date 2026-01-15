d4e75017d07bc5c0326c576aa4cc2fbdc7317a15
LucasBonato <lucas.perez.bonato@gmail.com>
2026-01-15 02:28:49 -0300

    [FEAT] Create IntegrationRouter that uses the IntegrationKey
    -Integration router will resolve the needed services instead of the controller
    -Removing the coupling of integrations from the core with the router resolver
    -Custom Exception for Unknown integrations
    -Implemented the IIntegrationRouter on OrdersHubController removing the direct ServiceProvider injection

---
+++ Src/Orders.Integrations.Hub/Core/Application/Integration/IIntegrationRouter.cs
@@ -0,0 +1,7 @@
+﻿namespace Orders.Integrations.Hub.Core.Application.Integration;
+
+public interface IIntegrationRouter {
+    TUseCase Resolve<TUseCase>(IntegrationKey key) where TUseCase : notnull;
+
+    bool CanResolve<TUseCase>(IntegrationKey key) where TUseCase : notnull;
+}
