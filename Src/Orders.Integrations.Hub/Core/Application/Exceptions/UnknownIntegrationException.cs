c455af0314944a3667cdb88613d1232170689e91
LucasBonato <lucas.perez.bonato@gmail.com>
2026-01-16 01:33:38 -0300

    [FIX] Custom Exception that is showing to much information from internal application

--- Src/Orders.Integrations.Hub/Core/Application/Exceptions/UnknownIntegrationException.cs
+++ Src/Orders.Integrations.Hub/Core/Application/Exceptions/UnknownIntegrationException.cs
@@ -8,14 +8,13 @@ using Orders.Integrations.Hub.Core.Domain.Contracts;
 namespace Orders.Integrations.Hub.Core.Application.Exceptions;
 
 public class UnknownIntegrationException(
-    Type useCaseType,
     IntegrationKey key
-) : Exception($"The integration of type <{useCaseType}> using the key <{key}> is unknown!"), IProblemDetailsException {
+) : Exception($"The integration of type using the key <{key.Value}> is unknown!"), IProblemDetailsException {
     private static HttpStatusCode StatusCode => HttpStatusCode.UnprocessableEntity;
 
     public ProblemDetails ToProblemDetails() {
         return new ProblemDetails {
-            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.2",
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5",
             Title = "The integration is unknown",
             Status = (int)StatusCode,
             Detail = Message,
