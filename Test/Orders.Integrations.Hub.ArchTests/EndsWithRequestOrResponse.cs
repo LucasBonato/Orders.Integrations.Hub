6e17171e678fc4efd069980d1700c221a5bdeaec
LucasBonato <lucas.perez.bonato@gmail.com>
2026-01-17 10:19:41 -0300

    [TEST] Add some Architecture tests using NetArchTest

---
+++ Test/Orders.Integrations.Hub.ArchTests/EndsWithRequestOrResponse.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Cecil;
+
+using NetArchTest.Rules;
+
+namespace Orders.Integrations.Hub.ArchTests;
+
+public class EndsWithRequestOrResponse : ICustomRule
+{
+    public bool MeetsRule(TypeDefinition type)
+    {
+        return type.Name.EndsWith("Request") || type.Name.EndsWith("Response");
+    }
+}
