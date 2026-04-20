7b7ecb303957ae0957ce670e67236e9d66f0ce1f
LucasBonato <lucas.perez.bonato@gmail.com>
2026-04-19 22:33:19 -0300

    refactor/integrations: Create IEndpoint and convert Integrations endpoints

---
+++ Src/Orders.Integrations.Hub/Core/Adapters/In/Http/Endpoint/IEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Orders.Integrations.Hub.Core.Adapters.In.Http.Endpoint;
+
+public interface IEndpoint
+{
+    void MapEndpoint(IEndpointRouteBuilder app);
+}
---
+++ Src/Orders.Integrations.Hub/Integrations/Rappi/Adapter/RappiWebhookPatchOrderEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Orders.Integrations.Hub.Core.Adapters.In.Http.Endpoint;
+using Orders.Integrations.Hub.Core.Application.Ports.In.UseCases;
+using Orders.Integrations.Hub.Integrations.Common.Middleware;
+using Orders.Integrations.Hub.Integrations.Rappi.Domain.ValueObjects.DTOs.Request;
+using Orders.Integrations.Hub.Integrations.Rappi.Infrastructure;
+
+using static Microsoft.AspNetCore.Http.Results;
+
+namespace Orders.Integrations.Hub.Integrations.Rappi.Adapter;
+
+internal sealed class RappiWebhookPatchOrderEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/Rappi/Webhook/Other", async (
+            [FromServices] IOrderUpdateUseCase<RappiWebhookEventOrderRequest> orderUpdate,
+            HttpContext context
+        ) => {
+            RappiWebhookEventOrderRequest request = (RappiWebhookEventOrderRequest)context.Items["WebhookRequest"]!;
+            await orderUpdate.ExecuteAsync(request);
+            return Accepted();
+        })
+        .WithTags("Rappi")
+        .WithDescription("Rappi Webhook Endpoint")
+        .ProducesValidationProblem()
+        .AddEndpointFilter<WebhookSignatureFilter<RappiWebhookEventOrderRequest, RappiSignatureValidator, RappiOrderEventResolver>>();
+    }
+}
